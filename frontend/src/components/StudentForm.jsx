94e924925c66f711d29906fc1a425f65510b082a
Rahulghodeswar <rahulghodeswar99@gmail.com>
2026-02-18 23:38:06 +0530

    Member 2

---
+++ frontend/src/components/StudentForm.jsx
@@ -0,0 +1,23 @@
+const [formData, setFormData] = useState({
+  name: "",
+  gender: "",
+  dob: "",
+  school_name: "",
+  mobile: "",
+  email: "",
+  house_no: "",
+  street: "",
+  city: "",
+  district: "",
+  state: "",
+  pin_code: "",
+  admission_date: "",
+  academic_year: "",
+  current_status: "Active",
+  class_id: "",
+
+  parent_details: {},
+  academic_details: {},
+  financial_details: {},
+  performance_details: {}
+});
